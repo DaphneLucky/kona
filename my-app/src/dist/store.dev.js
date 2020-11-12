34099cdc39916345d47ac7ead3deb77137162d98
Daphne Lucky <dlucky@student.unimelb.edu.au>
2020-11-12 22:15:11 +1100

    Add persisting state for store through refreshes.
    Source: https://youtu.be/o_Ni4Cqh4XA

--- my-app/src/dist/store.dev.js
+++ my-app/src/dist/store.dev.js
@@ -15,8 +15,35 @@ var _reducers = _interopRequireDefault(require("./reducers"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function saveToLocalStorage(state) {
+  try {
+    var serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+;
+
+function loadFromLocalStorage() {
+  try {
+    var serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+
+;
 var initialState = {};
 var middleware = [_reduxThunk["default"]];
-var store = (0, _redux.createStore)(_reducers["default"], initialState, (0, _reduxDevtoolsExtension.composeWithDevTools)(_redux.applyMiddleware.apply(void 0, middleware)));
+var persistedState = loadFromLocalStorage();
+var store = (0, _redux.createStore)(_reducers["default"], persistedState, (0, _reduxDevtoolsExtension.composeWithDevTools)(_redux.applyMiddleware.apply(void 0, middleware)));
+store.subscribe(function () {
+  return saveToLocalStorage(store.getState());
+});
 var _default = store;
 exports["default"] = _default;
--- my-app/src/store.js
+++ my-app/src/store.js
@@ -3,13 +3,36 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+function saveToLocalStorage(state) {
+    try {
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('state', serializedState)
+    } catch(e) {
+        console.log(e)
+    }
+};
+
+function loadFromLocalStorage() {
+    try {
+        const serializedState = localStorage.getItem('state')
+        if(serializedState === null) return undefined
+        return JSON.parse(serializedState)
+    } catch(e) {
+        console.log(e)
+        return undefined
+    }
+};
+
 const initialState = {};
 const middleware = [thunk];
+const persistedState = loadFromLocalStorage();
 
 const store = createStore(
     rootReducer,
-    initialState,
+    persistedState,
     composeWithDevTools(applyMiddleware(...middleware))
 );
+
+store.subscribe(() => saveToLocalStorage(store.getState())); 
 
 export default store;
