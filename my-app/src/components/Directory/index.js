6a484141690bca160c5e6e571cd81c8802660d52
Daphne Lucky <dlucky@student.unimelb.edu.au>
2020-11-13 11:25:58 +1100

    Remove carousel in Directory

--- my-app/src/components/Directory/index.js
+++ my-app/src/components/Directory/index.js
@@ -21,51 +21,12 @@ const Directory = props => {
     console.log(props);
     return (
         <div className="directory">
-            {/* <div className="wrap"> */}
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                        className="slide"
-                        src="https://images.unsplash.com/photo-1513530176992-0cf39c4cbed4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                        alt="First slide"
-                        />
-                        <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="slide"
-                        src="https://images.unsplash.com/photo-1513530176992-0cf39c4cbed4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                        alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="slide"
-                        src="https://images.unsplash.com/photo-1513530176992-0cf39c4cbed4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                        alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    </Carousel>
 
                 <div className="item">
                 <CardDeck>
                     <Card>
                         <div className="card_img">
-                            <Card.Img variant="top" src={Slide2} style={{width: '18rem',
-                height: '18rem'}}/>
+                            <Card.Img variant="top" src={Slide2} style={{width: '18rem', height: '18rem'}}/>
                         </div>
                         <Card.Body>
                         <Card.Title>Coffee + Creamer + Sugar</Card.Title>
@@ -81,8 +42,7 @@ const Directory = props => {
                         </Card.Body>
                     </Card>
                     <Card>
-                        <Card.Img variant="top" className="card_img" src={Slide1} style={{width: '18rem',
-                height: '18rem'}}/>
+                        <Card.Img variant="top" className="card_img" src={Slide1} style={{width: '18rem', height: '18rem'}}/>
                         <Card.Body>
                         <Card.Title>Coffee + Creamer + Sugar</Card.Title>
                         <Card.Text>
@@ -95,26 +55,6 @@ const Directory = props => {
                 </CardDeck>     
 
                 </div>
-                {/* <div
-                    className="item"
-                    style={{
-                        backgroundImage: `url(${ShopLeft})`
-                    }}
-                >
-                    <a>
-                        Shop Original
-                    </a>
-                </div>
-                <div
-                    className="item"
-                    style={{backgroundImage: `url(${ShopRight})`}}
-                >
-                    <a>
-                        Shop Matcha
-                    </a>
-                </div> */}
-
-            {/* </div> */}
         </div> 
     );
 };
