ca1d06b24dddf9a02faec8f2f9855f3a1a7a11b4
Daphne Lucky <dlucky@student.unimelb.edu.au>
2021-01-12 17:54:51 +1100

    Add images, styling to the steps and about us pages

--- my-app/src/pages/AboutUs/index.js
+++ my-app/src/pages/AboutUs/index.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
+import {Image, Container, Row, Col} from 'react-bootstrap';
 
 import './styles.scss';
+
+import pic1 from '../../assets/kona_people/1.jpg';
+import pic2 from '../../assets/kona_people/2.jpg';
+import pic3 from '../../assets/kona_people/3.jpg';
+import pic4 from '../../assets/kona_people/4.jpg';
+import pic5 from '../../assets/kona_people/5.JPG';
+import pic6 from '../../assets/kona_people/6.JPG';
 
 const AboutUs = props => {
     
@@ -11,7 +19,30 @@ const AboutUs = props => {
                 </h3>
                 <h4>We are coffee enthusiasts based in Melbourne. We believe in the future of coffee and how coffee can put a smile in peoples' faces. </h4>
                 <h3>#KONAPeople</h3>
-                
+                <Container>
+                    <Row>
+                        <Col sm={4}>
+                            <Image src={pic1} className="images" rounded />
+                        </Col>
+                        <Col sm={4}>
+                            <Image src={pic3} className="images" rounded />
+                        </Col>
+                        <Col sm={4}>
+                            <Image src={pic2} className="images" rounded />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={4}>
+                            <Image src={pic4} className="images" rounded />
+                        </Col>
+                        <Col sm={4}>
+                            <Image src={pic5} className="images" rounded />
+                        </Col>
+                        <Col sm={4}>
+                            <Image src={pic6} className="images" rounded />
+                        </Col>
+                    </Row>
+                 </Container>
             </div>
         );
 }
--- my-app/src/pages/HowToEnjoy/index.js
+++ my-app/src/pages/HowToEnjoy/index.js
@@ -1,4 +1,10 @@
 import React, {Component} from 'react';
+import {Card, Button, Carousel, CardColumns, DropdownButton, Dropdown} from 'react-bootstrap';
+
+import step1 from '../../assets/step1.png';
+import step2Cold from '../../assets/step2_cold.png';
+import step2Hot from '../../assets/step2_hot.png';
+import step3 from '../../assets/step3.png';
 
 import './styles.scss';
 
@@ -6,14 +12,36 @@ const HowToEnjoy = props => {
     
     return (
         <div className="steps">
+            <div id="bg"></div>
             <h3>
-                3 EASY STEPS TO ENJOY YOUR KONA
+                3 EASY STEPS
             </h3>
-            <h4>Step 1 - Put 2.5tbsp (30gr) of KONA</h4>
-            <h4>Step 2 -  Pick HOT or COLD</h4>
-            <h5>HOT: Add 250ml hot water</h5>
-            <h5>COLD: Add 200ml hot water + 2 ice cubes</h5>
-            <h4>Step 3 - Enjoy your KONA</h4>
+            <div className="card_wrap">
+            <Card className="card_style">
+                <Card.Title className="card_title">Step 1 - Put 2.5tbsp (30gr) of KONA</Card.Title>
+                <div className="border"></div> 
+                <Card.Img src={step1} className="card_image"/>
+            </Card>
+            <Card className="card_style">
+                <Card.Title className="card_title">Step 2 -  Pick HOT or COLD</Card.Title>
+                <div className="border"></div> 
+                <div className="step2_wrap">
+                    <div className="hot">
+                        <Card.Img src={step2Hot} className="card_image"/>
+                        <Card.Subtitle className="card_title">HOT: Add 250ml hot water</Card.Subtitle>
+                    </div>
+                    <div className="cold">
+                        <Card.Img src={step2Cold} className="card_image"/>
+                        <Card.Subtitle className="card_title">COLD: Add 200ml hot water + 2 ice cubes</Card.Subtitle>
+                    </div>
+                </div>
+            </Card>
+            <Card className="card_style">
+                <Card.Title className="card_title">Step 3 - Enjoy your KONA</Card.Title>
+                <div className="border"></div> 
+                <Card.Img src={step3} className="card_image"/>
+            </Card>
+            </div>
             <h6>Serving tips:</h6>
             <ul>
                 <li>Add milk and decrease the amount of water accordingly for extra taste</li>
